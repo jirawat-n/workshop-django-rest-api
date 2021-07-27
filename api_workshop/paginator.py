3619394039cee8df31bedc55f9da200889d3479e
Jirawat Nuanjun <86031118+jirawat777@users.noreply.github.com>
2021-07-27 09:31:54 +0700

    edit pagination

--- api_workshop/paginator.py
+++ api_workshop/paginator.py
@@ -3,7 +3,7 @@ import sys
 
 
 class CustomPagination(pagination.PageNumberPagination):
-    page_size = 10
+    page_size = 15
     page_size_query_param = 'page_size'
     max_page_size = 50
     page_query_param = 'page'
